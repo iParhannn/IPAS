93b5b08920877ce7147d7a03ed81e7632e916b73
BallSpins <sandalupil@gmail.com>
2023-08-27 17:34:21 +0700

    a

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+var mamalia = document.getElementsByClassName('canvas1')[0];
+var aves = document.getElementsByClassName('canvas2')[0];
+var reptil = document.getElementsByClassName('canvas3')[0];
+var amfibi = document.getElementsByClassName('canvas4')[0];
+var pisces = document.getElementsByClassName('canvas5')[0];
+
 function mamalia() {
-    var x = document.getElementById('mal');
-    for(var i = 0; i < x.length; i+=1) {
-        x[i].style.display = 'block';
-    }
+    mamalia.style.visibility='visible';
 }
