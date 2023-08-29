e53e4757ee7d4c17461deb02e81953a422fb562a
BallSpins <sandalupil@gmail.com>
2023-08-29 18:50:25 +0700

    don rek

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var awal = document.getElementsByClassName('canvas0')[0];
 var mamalia = document.getElementsByClassName('canvas1')[0];
 var aves = document.getElementsByClassName('canvas2')[0];
 var reptil = document.getElementsByClassName('canvas3')[0];
@@ -8,8 +9,10 @@ var crdt = document.getElementsByClassName('canvas6')[0];
 function Fmamalia() {
     if(mamalia.style.visibility == 'visible') {
         mamalia.style.visibility='hidden';
+        awal.style.visibility='visible';
     } else {
         mamalia.style.visibility='visible';
+        awal.style.visibility='hidden';
     }
     crdt.style.visibility='hidden';
     aves.style.visibility='hidden';
@@ -20,9 +23,11 @@ function Fmamalia() {
 function Faves() {
     if(aves.style.visibility == 'visible') {
         aves.style.visibility='hidden';
-    } else [
+        awal.style.visibility='visible';
+    } else {
         aves.style.visibility='visible'
-    ]
+        awal.style.visibility='hidden';
+    }
     crdt.style.visibility='hidden';
     mamalia.style.visibility='hidden';
     reptil.style.visibility='hidden';
@@ -32,8 +37,10 @@ function Faves() {
 function Freptil() {
     if(reptil.style.visibility == 'visible') {
         reptil.style.visibility='hidden'
+        awal.style.visibility='visible';
     } else {
-        reptil.style.visibility='visible'
+        reptil.style.visibility='visible';
+        awal.style.visibility='hidden';
     }
     crdt.style.visibility='hidden';
     mamalia.style.visibility='hidden';
@@ -43,9 +50,11 @@ function Freptil() {
 }
 function Famfibi() {
     if(amfibi.style.visibility == 'visible') {
-        amfibi.style.visibility='hidden'
+        amfibi.style.visibility='hidden';
+        awal.style.visibility='visible';
     } else {
-        amfibi.style.visibility='visible'
+        amfibi.style.visibility='visible';
+        awal.style.visibility='hidden';
     }
     crdt.style.visibility='hidden';
     mamalia.style.visibility='hidden';
@@ -55,9 +64,11 @@ function Famfibi() {
 }
 function Fpisces() {
     if(pisces.style.visibility == 'visible') {
-        pisces.style.visibility='hidden'
+        pisces.style.visibility='hidden';
+        awal.style.visibility='visible';
     } else {
-        pisces.style.visibility='visible'
+        pisces.style.visibility='visible';
+        awal.style.visibility='hidden';
     }
     mamalia.style.visibility='hidden';
     aves.style.visibility='hidden';
@@ -68,9 +79,11 @@ function Fpisces() {
 }
 function fCredit() {
     if(crdt.style.visibility == 'visible') {
-        crdt.style.visibility='hidden'
+        crdt.style.visibility='hidden';
+        awal.style.visibility='visible';
     } else {
-        crdt.style.visibility='visible'
+        crdt.style.visibility='visible';
+        awal.style.visibility='hidden';
     }
     mamalia.style.visibility='hidden';
     aves.style.visibility='hidden';
